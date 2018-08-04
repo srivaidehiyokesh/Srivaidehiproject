794d1e21f7feade99475c2f776c961bf3d1012b0
Yokesh Anand <Yokesh Anand@homepc>
2018-08-04 18:25:53 +0530

    updated code

--- src/main/java/week7/day1/GitCheck.java
+++ src/main/java/week7/day1/GitCheck.java
@@ -4,7 +4,7 @@ public class GitCheck {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		int sum,b1;//initialized by vaidehi
 		System.out.println("Git Check");
 
 	}
